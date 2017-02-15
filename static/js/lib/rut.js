9c6e7ab817257d681445ba87b515d58e358afee3
Orlando Vázquez <orlando.vazquez@cosof.cl>
2017-02-15 09:45:23 -0300

    añadida libreria formateo rut js

--- static/js/lib/rut.js
+++ static/js/lib/rut.js
@@ -1,32 +1,3 @@
-$(document).ready(function() {
-    $("#enviar").on('click', function(e) {
-        var button_process = buttonStartProcess($(this), e);
-        var parametros = $("#form").serialize();
-        $.ajax({         
-            dataType: "json",
-            cache:false,
-            async: true,
-            data: parametros,
-            type: "post",
-            url: BASE_URI + "index.php/Login/recuperar_password_rut", 
-            error: function(xhr, textStatus, errorThrown){
-
-            },
-            success:function(data){
-                buttonEndProcess(button_process);
-                if(data.correcto){
-                    $("#form-contenedor").addClass("hidden");
-                    $("#form-success").removeClass("hidden");
-                    $("#mensaje-modificacion").html("Se han enviado los datos para recuperar contraseña al correo del rut usuario: <strong>" + data.rut + "</strong>");
-                } else {
-                    procesaErrores(data.error);
-                    $("#form-error").removeClass("hidden");
-                }
-            }
-        }); 
-    }); 
-});
-
 //Formateo Rut
 function formateaRut(rut0)
 {
