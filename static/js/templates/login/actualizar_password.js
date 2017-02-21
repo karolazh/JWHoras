d2022c8c29844c90995d1a2bf3fe38c4017ee818
Victor Retamal <victor.retamales@redsalud.gov.cl>
2017-02-21 16:42:24 -0300

    - add msj error al actualizar password

--- static/js/templates/login/actualizar_password.js
+++ static/js/templates/login/actualizar_password.js
@@ -1,33 +1,34 @@
 $(document).ready(function() {
+
     $("#guardar").on('click', function(e) {
-        var button_process = buttonStartProcess($(this), e);
-        var parametros = $("#form").serialize();
+        var button_process	= buttonStartProcess($(this), e);
+        var parametros		= $("#form").serialize();
         $.ajax({         
-            dataType: "json",
-            cache:false,
-            async: true,
-            data: parametros,
-            type: "post",
-            url: BASE_URI + "index.php/Login/ajax_guardar_nuevo_password", 
-            error: function(xhr, textStatus, errorThrown){
+            dataType	: "json",
+            cache		: false,
+            async		: true,
+            data		: parametros,
+            type		: "post",
+            url			: BASE_URI + "index.php/Login/ajax_guardar_nuevo_password", 
+            error		: function(xhr, textStatus, errorThrown){
 
             },
-            success:function(data){
+            success		: function(data){
                 buttonEndProcess(button_process);
                 if(data.correcto){
                     $("#password").val("");
                     $("#password_repetido").val("");
-                    //$("#form-ok").removeClass("hidden");
-                    //location.href = BASE_URI + "index.php/Home/dashboard";
                     limpiaErrores(data.error);
                     $("#form-error").addClass("hidden");
                     xModal.info('Contraseña Actualizada').freeze();
                     location.href = BASE_URI + "index.php/Login/actualizar";
                 } else {
                     procesaErrores(data.error);
+					xModal.info('Error al Actualizar la Contraseña.').freeze();
                     $("#form-error").removeClass("hidden");
                 }
             }
         }); 
     });
+
 });
