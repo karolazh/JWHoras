209b17eb79aefaff242d6a047d51cd1fa73ca904
Victor Retamal <victor.retamales@redsalud.gov.cl>
2017-02-22 10:39:39 -0300

    - comentar función repetida getRegistro(), se debe usar getRegistroById

--- app/models/DAORegistro.php
+++ app/models/DAORegistro.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 class DAORegistro extends Model{
 
     protected $_tabla			= "pre_registro";
@@ -16,7 +14,6 @@ class DAORegistro extends Model{
     /*
      * Lista Registro
      */
-
     public function getListaRegistro(){
         $query	= "	SELECT
 						id_registro,
@@ -34,58 +31,13 @@ class DAORegistro extends Model{
         }
     }
 
-	/*
-	public function getListaRegistroById($id_registro){
-        $query	= "	SELECT 
-						date_format(r.reg_fec_ingreso,'%d-%m-%Y') as reg_fec_ingreso,
-						date_format(r.reg_hora_ingreso,'%H:%i:%s') as reg_hora_ingreso,
-						r.reg_motivo_consulta,
-						r.reg_historia_enfermedad,
-						r.reg_diagnostico,
-						r.reg_indicacion_medica
-					FROM tab_registro r 
-						INNER JOIN tab_pacientes p ON p.pac_id= r.reg_pac_id 
-					WHERE p.pac_id = ? " ;
-
-		$param		= array($id_registro);
-        $resultado	= $this->db->getQuery($query,$param);
-
-        if($resultado->numRows>0){
-            return $resultado->rows;
-        } else {
-            return NULL;
-        }
-    }
-
-	*/
     public function getRegistroById($id_registro) {
-        $query = "select "
-                . "id_registro, "
-                . "gl_rut, "
-                . "bo_extranjero, "
-                . "gl_run_pass, "
-                . "gl_nombres, "
-                . "gl_apellidos, "
-                . "date_format(fc_nacimiento,'%d-%m-%Y') as fc_nacimiento, "
-                . "gl_sexo, "
-                . "id_prevision, "
-                . "gl_direccion, "
-                . "id_comuna, "
-                . "gl_fono, "
-                . "gl_celular, "
-                . "gl_email, "
-                . "fc_actualiza, "
-                . "gl_latitud, "
-                . "gl_longitud, "
-                . "bo_reconoce, "
-                . "bo_acepta_programa, "
-                . "id_adjunto, "
-                . "id_estado_caso, "
-                . "id_institucion,"
-                . "id_usuario_crea, "
-                . "fc_crea "
-                . "from pre_registro "
-                . "where id_registro = ?";
+        $query	= "	SELECT
+						pre_registro.*,
+						date_format(fc_nacimiento,'%d-%m-%Y') as fc_nacimiento
+					FROM pre_registro 
+					WHERE id_registro = ?";
+
         $param = array($id_registro);
         $consulta = $this->db->getQuery($query, $param);
 
@@ -95,10 +47,9 @@ class DAORegistro extends Model{
             return null;
         }
     }
-    /*
-     * Ver Registro
-     */	
-    //funcion repetida
+   
+    //funcion repetida, usar getRegistroById
+	/*
     public function getRegistro($id_registro){
         $query	= "	SELECT
 						pre_registro.*,
@@ -115,26 +66,25 @@ class DAORegistro extends Model{
             return null;
         }
     }
+	*/
 
     public function getRegistroByRut($rut_registro) {
 
-        $query = "	SELECT 
-                            id_registro,
-                            gl_nombres,
-                            gl_apellidos,
-                            date_format(fc_nac,'%d-%m-%Y') as fc_nac,
-                            id_prevision,
-                            gl_direccion,
-                            gl_fono,
-                            gl_email,
-                            gl_celular
-                        FROM pre_registro 
-                        WHERE gl_rut = ?";
+        $query	= "	SELECT 
+						id_registro,
+						gl_nombres,
+						gl_apellidos,
+						date_format(fc_nac,'%d-%m-%Y') as fc_nac,
+						id_prevision,
+						gl_direccion,
+						gl_fono,
+						gl_email,
+						gl_celular
+					FROM pre_registro 
+					WHERE gl_rut = ?";
 
-
-        $param = array($rut_registro);
-        $consulta = $this->db->getQuery($query, $param);
-
+        $param		= array($rut_registro);
+        $consulta	= $this->db->getQuery($query, $param);
 
         if ($consulta->numRows > 0) {
             return $consulta->rows->row_0;
