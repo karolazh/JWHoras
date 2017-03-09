3b08191cdb6571c4f6a10a5e6c4d399f3d803306
Orlando Vázquez <orlando.vazquez@cosof.cl>
2017-03-09 00:38:15 -0300

    Agregados eventos a Medico.php

--- app/controllers/Medico.php
+++ app/controllers/Medico.php
@@ -14,7 +14,7 @@
  * --------------
  * !cProgramador				!cFecha		!cDescripcion 
  * -----------------------------------------------------------------------------
- * 
+ *<orlando.vazquez@cosof.cl>	08-03-2017	Agregada maqueta
  * -----------------------------------------------------------------------------
  * ****************************************************************************
 */
@@ -23,12 +23,15 @@ class Medico extends Controller {
 
 	protected $_DAOPaciente;
 	protected $_DAOEmpa;
+	protected $_Evento;
 
 	function __construct() {
 		parent::__construct();
 		$this->load->lib('Fechas', false);
 		$this->load->lib('Boton', false);
 		$this->load->lib('Seguridad', false);
+		$this->load->lib('Evento', false);
+		$this->_Evento = new Evento();
 
 		$this->_DAOPaciente				= $this->load->model("DAOPaciente");
 		$this->_DAOEmpa					= $this->load->model("DAOEmpa");
@@ -45,6 +48,13 @@ class Medico extends Controller {
 
 		$this->_display('Paciente/index.tpl');
 		$this->load->javascript(STATIC_FILES . "js/templates/Paciente/index.js");
+	}
+	public function plan_tratamiento(){
+		print_r("Dentro de 'plan_tratamiendo'");
+		$resp = $this->_Evento->guardarMostrarUltimo(20,0,0,"Plan tratamiento Iniciado el : " . Fechas::fechaHoyVista(),1,1,$_SESSION['id']);
+		$resp = $this->_Evento->guardarMostrarUltimo(21,0,0,"Plan tratamiento Modificado el : " . Fechas::fechaHoyVista(),1,1,$_SESSION['id']);
+		DIE();
+		
 	}
 
 }
