e3b801c63c581d582a2fbc43885f3363df7ba0a5
Victor <victor.retamales@redsalud.gob.cl>
2017-03-29 00:49:00 -0400

    - Botones v 2.0

--- app/libs/Boton.php
+++ app/libs/Boton.php
@@ -35,83 +35,114 @@ Class Boton{
 				</span>';
 	}
 
-	/**
-	* Generar botones en Grilla
-	* @param string $bandeja [de donde se llama]
-	* @return string html
-	*/
-	public static function botonGrillaPaciente($bandeja='',$id_paciente='0'){
-		$id_perfil		= $_SESSION['perfil'];
-		$id_region		= $_SESSION['id_region'];
-		$id_institucion	= $_SESSION['id_institucion'];
-		$id_laboratorio = $_SESSION['id_laboratorio'];
-
-		$empa 	= "	<button type='button' 
+	public static function getBotonEmpa($id_paciente='0'){
+		return	"	<button type='button' 
 						onClick=\"location.href=('".BASE_URI."/Empa/nuevo/$id_paciente')\" 
 						data-toggle='tooltip' 
 						class='btn btn-xs btn-success'
 						data-title='Formulario EMPA'>
 						<i class='fa fa-book'></i>
 					</button>";
+	}
 
-		$plan 	= "	<button type='button' 
+	public static function getBotonPlan($id_paciente='0'){
+		return	"	<button type='button' 
 						onClick=\"location.href=('".BASE_URI."/Medico/plan_tratamiento/$id_paciente')\" 
 						data-toggle='tooltip' 
 						class='btn btn-xs btn-default'
 						data-title='Plan Tratamiento'>
 						<i class='fa fa-medkit'></i>
 					</button>";
+	}
 
-		$diagnostico 	= "	<button type='button' 
-								onClick=\"location.href=('".BASE_URI."/Especialista/diagnostico/$id_paciente')\" 
-								data-toggle='tooltip' 
-								class='btn btn-xs btn-default'
-								data-title='Diagnóstico'>
-								<i class='fa fa-user-md'></i>
-							</button>";
+	public static function getBotonDiagnostico($id_paciente='0'){
+		return	"	<button type='button' 
+						onClick=\"location.href=('".BASE_URI."/Especialista/diagnostico/$id_paciente')\" 
+						data-toggle='tooltip' 
+						class='btn btn-xs btn-default'
+						data-title='Diagnóstico'>
+						<i class='fa fa-user-md'></i>
+					</button>";
+	}
 
-		$seguimiento 	= "	<button type='button' 
-								onClick=\"location.href=('".BASE_URI."/Gestor/seguimiento/$id_paciente')\" 
-								data-toggle='tooltip' 
-								class='btn btn-xs btn-warning'
-								data-title='Seguimiento'>
-								<i class='fa fa-eye'></i>
-							</button>";
+	public static function getBotonSeguimiento($id_paciente='0'){
+		return	"	<button type='button' 
+						onClick=\"location.href=('".BASE_URI."/Gestor/seguimiento/$id_paciente')\" 
+						data-toggle='tooltip' 
+						class='btn btn-xs btn-warning'
+						data-title='Seguimiento'>
+						<i class='fa fa-eye'></i>
+					</button>";
+	}
 
-		$examen 	= "	<button type='button' 
-								onClick=\"location.href=('".BASE_URI."/Laboratorio/ver/$id_paciente')\" 
-								data-toggle='tooltip' 
-								class='btn btn-xs btn-success'
-								data-title='Formulario Examen'>
-								<i class='fa fa-file-text-o'></i>
-							</button>";
+	public static function getBotonExamen($id_paciente='0'){
+		return	"	<button type='button' 
+						onClick=\"location.href=('".BASE_URI."/Laboratorio/ver/$id_paciente')\" 
+						data-toggle='tooltip' 
+						class='btn btn-xs btn-success'
+						data-title='Formulario Examen'>
+						<i class='fa fa-file-text-o'></i>
+					</button>";
+	}
 
-		$agendaExamen 	= "	<button type='button' 
-								onClick=\"xModal.open('".BASE_URI."/Agenda/ver/$id_paciente', 'Agenda Examen Paciente', 85)\" 
-								data-toggle='tooltip' 
-								class='btn btn-xs btn-info'
-								data-title='Ver Agenda Examen'>
-								<i class='fa fa-calendar'></i>
-							</button>";
-		// Falta Ver Agenda Especialista
+	public static function getBotonAgendaExamen($id_paciente='0'){
+		return	"	<button type='button' 
+						onClick=\"xModal.open('".BASE_URI."/Agenda/ver/$id_paciente', 'Agenda Examen Paciente', 85)\" 
+						data-toggle='tooltip' 
+						class='btn btn-xs btn-info'
+						data-title='Ver Agenda Examen'>
+						<i class='fa fa-calendar'></i>
+					</button>";
+	}
 
-		$ver	= "	<button type='button' 
+	public static function getBotonAgendaEspecialista($id_paciente='0'){
+		return	"	<button type='button' 
+						onClick=\"xModal.open('".BASE_URI."/Agenda/especialista/$id_paciente', 'Agenda Especialista', 85)\" 
+						data-toggle='tooltip' 
+						class='btn btn-xs btn-primary'
+						data-title='Ver Agenda Especialista'>
+						<i class='fa fa-calendar'></i>
+					</button>";
+	}
+
+	public static function getBotonVer($id_paciente='0'){
+		return	"	<button type='button' 
 						onClick=\"xModal.open('".BASE_URI."/Paciente/ver/$id_paciente', 'Detalle Paciente', 85)\" 
 						data-toggle='tooltip' 
 						class='btn btn-xs btn-info'
 						data-title='Ver Paciente'>
 						<i class='fa fa-search'></i>
 					</button>";
+	}
 
-		$bitacora	= "	<button type='button' 
+	public static function getBotonBitacora($id_paciente='0'){
+		return	"	<button type='button' 
 						onClick=\"xModal.open('".BASE_URI."/Bitacora/ver/$id_paciente', 'Bitácora Paciente', 85)\" 
 						data-toggle='tooltip' 
 						class='btn btn-xs btn-primary'
 						data-title='Bitácora'>
 						<i class='fa fa-info-circle'></i>
 					</button>";
+	}
 
-		$botones	= $agendaExamen.$ver.$bitacora;
+	/**
+	* Generar botones en Grilla
+	* @param string $bandeja [de donde se llama]
+	* @param int $id_paciente
+	* @return string html
+	*/
+	public static function botonGrillaPaciente($bandeja='',$id_paciente='0'){
+
+		$empa 					= Boton::getBotonEmpa($id_paciente);
+		$plan 					= Boton::getBotonPlan($id_paciente);
+		$diagnostico 			= Boton::getBotonDiagnostico($id_paciente);
+		$seguimiento			= Boton::getBotonSeguimiento($id_paciente);
+		$examen 				= Boton::getBotonExamen($id_paciente);
+		$agendaExamen 			= Boton::getBotonAgendaExamen($id_paciente);
+		$agendaEspecialista 	= Boton::getBotonAgendaEspecialista($id_paciente);
+		$ver					= Boton::getBotonVer($id_paciente);
+		$bitacora				= Boton::getBotonBitacora($id_paciente);
+		$botones				= $agendaExamen.$ver.$bitacora;
 
 		if($bandeja == 'Paciente'){
 			$botones	= $empa.$agendaExamen.$ver.$bitacora;
